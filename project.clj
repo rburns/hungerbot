568929f69697fbfc86d09f6bbc1de681a34aea3c
Rob Burns <rburns@paiges.net>
2014-12-30 12:14:44 +0700

    upgrade clojurescript

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2505"]]
+                 [org.clojure/clojurescript "0.0-2511"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
 
