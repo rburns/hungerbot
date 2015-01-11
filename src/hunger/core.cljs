50969f0833161eb92241c3afc995edad47c9353d
Rob Burns <rburns@paiges.net>
2015-01-11 15:41:59 +0700

    hunger api: change parameter order, add meta data support for feeds

--- src/hunger/core.cljs
+++ src/hunger/core.cljs
@@ -1,24 +1,39 @@
 (ns hunger.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.nodejs :as nodejs]
+            [cljs.core.async :refer [<!]]
             [hunger.store :refer [fetch write collection-fetch collection-add collection-remove]]))
 
+(defrecord Feed [url info])
+
+(defn url->feed
+  [url store]
+  (Feed. url {}))
+
 (defn add-feed
-  [url store cb]
-  (collection-add store "feeds" url cb))
+  [store feed cb]
+  (let [results (atom  [])]
+    (go (swap! results conj (<! (collection-add store "feeds" (:url feed))))
+        (doseq [[prop value] (seq (:info feed))]
+          (swap! results conj (<! (write store ["feed" (:url feed) (name prop)] value))))
+        (cb nil feed))))
 
 (defn remove-feed
-  [url store cb]
-  (collection-remove store "feeds" url cb))
+  [store feed cb]
+  (collection-remove store "feeds" feed))
 
 (defn list-feeds
-  [store cb]
-  (collection-fetch store "feeds" cb))
+  [store filter cb]
+  (let [results (atom [])]
+    (go
+      (swap! results conj (map #(url->feed % store) (<! (collection-fetch store "feeds"))))
+      (cb nil @results))))
 
 (defn last-item-in-feed
-  [url store cb]
-  (fetch store (str "last-item-of-" url) cb))
+  [store feed cb]
+  (fetch store (str "last-item-of-" feed)))
 
 (defn update-last-item-in-feed
-  [url item store cb]
-  (write store (str "last-item-of-" url) item cb))
+  [store feed item cb]
+  (write store (str "last-item-of-" feed) item))
 
