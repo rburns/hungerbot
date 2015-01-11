9f1f2507200a850d0ab3c0ca8198eb5ac1d83b28
Rob Burns <rburns@paiges.net>
2015-01-11 15:18:18 +0700

    redis wrapper for npm lib

---
+++ test/redis/core_test.cljs
@@ -0,0 +1,25 @@
+(ns test.redis.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.test :refer-macros [deftest testing is]]
+            [test.helpers :refer [unordered-equal]]
+            [cljs.core.async :refer [<!]]
+            [redis.core :as r]))
+
+(deftest sanity
+  (let [client (r/redis-connect)]
+    (go
+      (is (= "OK" (<! (client :set "key" "value"))))
+      (is (= "value" (<! (client :get "key"))))
+      (is (= 1 (<! (client :del "key"))))
+      (client :quit))))
+
+(deftest wildcard-keys
+  (let [client (r/redis-connect)]
+    (go
+      (is (= "OK" (<! (client :set "one:two" "value"))))
+      (is (= "OK" (<! (client :set "one:three" "value"))))
+      (is (unordered-equal ["one:three" "one:two"] (<! (client :keys "one:*"))))
+      (is (= 1 (<! (client :del "one:two"))))
+      (is (= 1 (<! (client :del "one:three"))))
+      (client :quit))))
+
