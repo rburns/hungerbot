49daac19b19af3fb151e8ecd895300ad3ac636a5
Rob Burns <rburns@paiges.net>
2015-01-12 18:29:41 +0700

    remove store atom

--- src/hungerbot/core.cljs
+++ src/hungerbot/core.cljs
@@ -10,26 +10,26 @@
 
 (def config (:config (js->clj (nodejs/require "../config.js") :keywordize-keys true)))
 
-(def store (atom nil))
-
 (defn Feed->desc
   [feed]
   (:url feed))
 
-(def subscribe-cmd
+(defn subscribe-cmd
+  [store]
   {:description "Add a feed to the current channel."
    :params [:url]
    :handler (fn [message slack]
               (let [channel (:channel message)]
                 (if-let [url (token->url (-> message :params first))]
                   (let [feed (Feed. url {:channel (.-name channel)})]
-                    (add-feed @store feed (fn [error, result]
-                                            (if (= nil error)
-                                              (.send channel "Duly noted, sir.")
-                                              (.send channel "Something went wrong.")))))
+                    (add-feed store feed (fn [error, result]
+                                           (if (= nil error)
+                                             (.send channel "Duly noted, sir.")
+                                             (.send channel "Something went wrong.")))))
                   (.send channel "Hardly seems you should be looking at that."))))})
 
-(def list-cmd
+(defn list-cmd
+  [store]
   {:description "List the feeds in the current channel."
    :handler  (fn [message slack]
                (let [sieve   (fn [feed] (= (.-name (:channel message)) (:channel (:info feed))))
@@ -39,9 +39,10 @@
                                 (.send (:channel message) "Got no feeds, sorry.")
                                 (.send (:channel message) (join "\n" (map Feed->desc result))))
                               (.send (:channel message) "Not sure what to say.")))]
-                 (list-feeds @store sieve done)))})
+                 (list-feeds store sieve done)))})
 
-(def remove-cmd
+(defn remove-cmd
+  [store]
   {:description "Removes a feed from the current channel."
    :params [:url]
    :handler (fn [message slack]
@@ -52,20 +53,20 @@
                     scrub (fn [error result]
                             (if (= nil result)
                               (.send (:channel message) "Uh, can't find that one.")
-                              (remove-feed @store result done)))]
-                (fetch-feed @store feed sieve scrub)))})
+                              (remove-feed store result done)))]
+                (fetch-feed store feed sieve scrub)))})
 
 (defn default-response
   [message slack]
   (.send (:channel message) "I have no idea what you are talking about."))
 
 (defn -main []
-  (reset! store (redis-store/store))
-  (carcass/animate {:description "I'll give you the feeds."
-                    :config config
-                    :commands {:subscribe subscribe-cmd
-                               :list list-cmd
-                               :remove remove-cmd}
-                    :default-response default-response}))
+  (let [store (redis-store/store)
+        bot   (carcass/animate {:description "I'll give you the feeds."
+                                :config config
+                                :commands {:subscribe (subscribe-cmd store)
+                                           :list (list-cmd store)
+                                           :remove (remove-cmd store)}
+                                :default-response default-response})]))
 
 (set! *main-cli-fn* -main)
